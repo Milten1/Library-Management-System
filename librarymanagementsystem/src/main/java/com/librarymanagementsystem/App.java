ac38586677f822caa4b7748761522626ab0aedf0
kamil <kamil.kajmowski@gmail.com>
2021-10-19 06:26:59 +0200

    Add basic UI

--- librarymanagementsystem/src/main/java/com/librarymanagementsystem/App.java
+++ librarymanagementsystem/src/main/java/com/librarymanagementsystem/App.java
@@ -1,14 +1,14 @@
 package com.librarymanagementsystem;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        TUI ui = new TUI();
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+    	
+    	ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    	
+        TUI ui = (TUI) context.getBean(TUI.class);
         ui.start();
     }
 }
--- librarymanagementsystem/src/main/java/com/librarymanagementsystem/TUI.java
+++ librarymanagementsystem/src/main/java/com/librarymanagementsystem/TUI.java
@@ -3,43 +3,66 @@ package com.librarymanagementsystem;
 import java.util.Scanner;
 
 public class TUI {
+	Logic logic = new Logic();
+	
+	
 	public void start() {
+		logic.loadBooksFromFile();
+		
 		System.out.println("Welcome to the Library Management System!");
 		System.out.println("Enter your command or type 'help' to show commands: ");
-		System.out.print("> ");
 		
-		Scanner scanner = new Scanner(System.in);
-		String command = scanner.next();
-		
-		String[] parts = command.split(" ");
-		
-		switch (parts[0]) {
-		case "help":{
-			System.out.println("Commands: \n"
-					+ "'help' - prints all commands \n"
-					+ "'login {username}' - login as {username} \n"
-					+ "'logout' - logouts \n"
-					+ "'add' - adds a book \n"
-					+ "'list' - lists all books \n"
-					+ "'delete' - deletes a book \n"
-					+ "'search' - searchs for a book \n"
-					+ "'adduser' - adds user [ADMIN COMMAND] \n"
-					+ "'deleteuser' - deletes user [ADMIN COMMAND] \n"
-					+ "'exit' - ends program runtime");
-			break;
+		while(true) {
+			System.out.print("> ");
+			
+			Scanner scanner = new Scanner(System.in);
+			String command = scanner.next();
+			if(command.equals("exit")) break;
+			
+			String[] parts = command.split(" ");
+			
+			switch (parts[0]) {
+			case "help":{
+				System.out.println("Commands: \n"
+						+ "'help' - prints all commands \n"
+						+ "'login {username}' - login as {username} \n"
+						+ "'logout' - logouts \n"
+						+ "'add' - adds a book \n"
+						+ "'list' - lists all books \n"
+						+ "'delete' - deletes a book \n"
+						+ "'search' - searchs for a book \n"
+						+ "'adduser' - adds user [ADMIN COMMAND] \n"
+						+ "'deleteuser' - deletes user [ADMIN COMMAND] \n"
+						+ "'exit' - ends program runtime");
+				break;
+			}
+			case "add":{
+				logic.add();
+				break;
+			}
+			case "delete":{
+				logic.delete();
+				break;
+			}
+			case "list":{
+				logic.list();
+				break;
+			}
+			
+			
+			
+			
+			
+			
+			default:{
+				System.out.println("Unknown command");
+				break;
+			}
+			}
 		}
 		
+		logic.saveBookToFile();
 		
 		
-		
-		
-		
-		
-		
-		default:{
-			System.out.println("Unknown command");
-			break;
-		}
-		}
 	}
 }
